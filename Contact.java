361f5fde0174624f809b59da60a5bc61b24e8e4a
Roque Busto <busto.rj@gmail.com>
2023-04-27 14:10:23 -0700

    Programming Assignment 4

---
+++ Contact.java
@@ -0,0 +1,24 @@
+public class Contact {
+    private String firstName;
+    private String lastName;
+    private long homeNumber;
+    private long officeNumber;
+    private String emailAddress;
+
+    /**
+     * Contructor setting all details.
+     * @param firstName
+     * @param lastName
+     * @param homeNumber
+     * @param officeNumber
+     * @param emailAddress
+     */
+    public Contact(String firstName, String lastName, long homeNumber, long officeNumber, String emailAddress){
+
+    }
+
+    // Setter //
+    // Getter //
+    // toString //
+
+}
