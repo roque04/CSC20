9a726e9c4d9a82a307838825c353f2c6403edf99
Roque Busto <busto.rj@gmail.com>
2023-04-27 14:40:33 -0700

    Programming Assignment 4

--- PhoneBook.java
+++ PhoneBook.java
@@ -1,3 +1,4 @@
+import java.io.FileNotFoundException;
 import java.util.ArrayList;
 
 /**
@@ -7,5 +8,46 @@ import java.util.ArrayList;
 public class PhoneBook{
     public ArrayList<Contact> contacts;
 
-    
+    /**
+     * create arraylist of contacts by reading data from the file
+     */
+    public PhoneBook(){
+
+    }
+    public PhoneBook(String phoneFile) throws FileNotFoundException {
+        
+    }
+    /**
+     * add contact to the array list
+     * @param c
+     */
+    public add(Contact c){
+
+    }
+    /**
+     * returns the entire phone book as string
+     */
+    public toString(){
+
+    }
+    /**
+     * Sort phonebook by first name
+     */
+    public void bubbleSort(){
+
+    }
+    /**
+     * Sort phone book by last using selection sort algorithm
+     */
+    public void selectionSort(){
+
+    }
+    /**
+     * Search contact by last name using binary search.
+     * @param name
+     * @return return true if contact is found else false.
+     */
+    public boolean binarySearch(String name){
+
+    }
 }
--- PhoneDriver.java
+++ PhoneDriver.java
@@ -1,9 +1,57 @@
+import java.io.FileNotFoundException;
+import java.util.InputMismatchException;
+import java.util.Scanner;
 
 /**
  * @author Roque Busto
  * Date: 4/27/2023
  */
 public class PhoneDriver {
+    private static String menu;
+    private static Scanner scan;
+    private static PhoneBook phoneBook;
     
-    
+    public static void phoneMenu(){
+        menu = "\n Contact Book Menu Options \n\n"+
+        "P: Print Phonebook\n"+
+        "B: Sort phone book by first name\n"+
+        "L: Sort phone book by last name\n"+
+        "S: Search contact by last name\n"+
+        "A: Add contact to phone book\n"+
+        "Q. Quit\n";
+
+        try {
+            phoneBook = new PhoneBook("ContactDetail.txt");
+            scan = new Scanner(System.in);
+            boolean running = true;
+
+            while(running){
+                System.out.println(menu);
+                System.out.print("Please enter your choice: ");
+                String choice = scan.nextLine().toUpperCase();
+
+                switch(choice){
+                    case "P":
+                        break;
+                    case "B":
+                        break;
+                    case "L":
+                        break;
+                    case "S":
+                        break;
+                    case "A":
+                        break;
+                    case "Q":
+                        running = false;
+                        break;
+                    default:
+                        throw new InputMismatchException("INCORRECT INPUT");
+                }
+            }
+        } catch (FileNotFoundException e){
+            System.out.println("File not found.");
+        }
+        System.out.println("Good bye!");
+    }
+
 }
